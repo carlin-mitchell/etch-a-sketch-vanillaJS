1a008474d53b29dce75fab616bc750acbd0768ac
Carlin Mitchell <carlinmi@umn.edu>
2023-02-07 08:59:43 -0600

    “first commit”

---
+++ main.js
@@ -0,0 +1,195 @@
+import {
+  getRandomRgbStr,
+  hexToRgbStr,
+  rgbStrToFullHex,
+  setElementsBgColor,
+  shadeRgbStrByFactor,
+} from "./utilities.js";
+
+const bodyBackgroundColor = "rgb(230, 230, 230)";
+const maxGridSize = 64;
+const minGridSize = 2;
+
+let currentGridBackgroundColor = bodyBackgroundColor;
+let currentGridSize = 16;
+let currentPaintColor = "rgb(75, 75, 75)";
+let gridLinesVisible = true;
+let leftMouseBtnDown = false;
+let rainbowModeEnabled = false;
+let shadeModeEnabled = false;
+
+const body = document.body;
+const currentYear = document.querySelector("#current-year");
+const eraseBtn = document.querySelector("#erase");
+const eraseGridBtn = document.querySelector("#erase-grid");
+const grid = document.querySelector("#grid");
+const gridColorPicker = document.querySelector("#grid-color-picker");
+const gridSizePicker = document.querySelector("#grid-sizer");
+const gridSizeValue = document.querySelector("#current-grid-size");
+const paintColorPicker = document.querySelector("#paint-color-picker");
+const toggleGridlinesBtn = document.querySelector("#toggle-gridlines");
+const toggleRainbowModeBtn = document.querySelector("#toggle-rainbow-mode");
+const toggleShadeModeBtn = document.querySelector("#toggle-shade-mode");
+
+eraseBtn.onclick = () => setPaintColorToErase();
+eraseGridBtn.onclick = () => eraseFullGrid();
+gridColorPicker.oninput = (event) => setFullGridColor(event);
+gridSizePicker.oninput = (event) => setGridSize(event);
+paintColorPicker.oninput = (event) => updatePaintColorFromPicker(event);
+toggleGridlinesBtn.onclick = () => toggleGridLines();
+toggleRainbowModeBtn.onclick = () => toggleRainbowMode("toggle");
+toggleShadeModeBtn.onclick = () => toggleShadeMode("toggle");
+window.onmousedown = () => (leftMouseBtnDown = true);
+window.onmouseup = () => (leftMouseBtnDown = false);
+window.onload = () => initializeAppState();
+
+function disableElements(elementsArr) {
+  elementsArr.forEach((element) => (element.disabled = true));
+}
+
+function enableElements(elementsArr) {
+  elementsArr.forEach((element) => (element.disabled = false));
+}
+function eraseFullGrid() {
+  setElementsBgColor(getAllGridSquares(), hexToRgbStr(gridColorPicker.value));
+}
+
+function getAllGridSquares() {
+  const gridSquares = document.querySelectorAll(".grid-item");
+  return [...gridSquares];
+}
+
+function generateGrid(size) {
+  for (let row = 0; row < size; row++) {
+    // generate rows based on the current grid size
+    let newRow = document.createElement("div");
+    newRow.classList.add("grid-row");
+    grid.appendChild(newRow);
+    for (let col = 0; col < size; col++) {
+      let newGridSquare = document.createElement("div");
+      newGridSquare.classList.add("grid-item");
+      newGridSquare.style.backgroundColor = hexToRgbStr(gridColorPicker.value);
+      newGridSquare.addEventListener("mousedown", paintSquare);
+      newGridSquare.addEventListener("mouseover", paintSquare);
+      newRow.appendChild(newGridSquare);
+    }
+  }
+}
+
+function initializeAppState() {
+  body.style.backgroundColor = bodyBackgroundColor;
+  gridColorPicker.value = rgbStrToFullHex(currentGridBackgroundColor);
+  paintColorPicker.value = rgbStrToFullHex(currentPaintColor);
+  gridSizePicker.max = maxGridSize;
+  gridSizePicker.min = minGridSize;
+  gridSizePicker.value = currentGridSize;
+  currentYear.innerText = " " + new Date().getFullYear();
+  generateGrid(currentGridSize);
+  updateGridSizeDisplay(currentGridSize);
+}
+
+function paintSquare(event) {
+  if (event.type === "mousedown" || leftMouseBtnDown) {
+    if (shadeModeEnabled) {
+      const currentSquareBgColor = this.style.backgroundColor;
+      currentPaintColor = shadeRgbStrByFactor(currentSquareBgColor, 0.9);
+    } else if (rainbowModeEnabled) {
+      currentPaintColor = getRandomRgbStr();
+    } else {
+      currentPaintColor = hexToRgbStr(paintColorPicker.value);
+    }
+
+    this.style.backgroundColor = currentPaintColor;
+  }
+}
+
+function setFullGridColor(event) {
+  currentGridBackgroundColor = event.target.value;
+  setElementsBgColor(
+    getAllGridSquares(),
+    hexToRgbStr(currentGridBackgroundColor)
+  );
+}
+
+function setGridSize(event) {
+  currentGridSize = event.target.value;
+  grid.replaceChildren();
+  generateGrid(currentGridSize);
+  updateGridSizeDisplay(currentGridSize);
+}
+
+function setPaintColorToErase() {
+  currentPaintColor = hexToRgbStr(gridColorPicker.value);
+  paintColorPicker.value = rgbStrToFullHex(currentPaintColor);
+  toggleRainbowMode("off");
+}
+
+function toggleGridLines() {
+  const gridSquares = document.querySelectorAll(".grid-item");
+  if (gridLinesVisible) {
+    gridSquares.forEach((square) => square.classList.add("borderless"));
+    gridLinesVisible = false;
+  } else {
+    gridSquares.forEach((square) => {
+      square.classList.remove("borderless");
+      gridLinesVisible = true;
+    });
+  }
+}
+
+function toggleRainbowMode(stateStr) {
+  switch (stateStr) {
+    case "on": {
+      rainbowModeEnabled = true;
+      toggleRainbowModeBtn.classList.add("rainbow-mode-active");
+      return;
+    }
+    case "off": {
+      rainbowModeEnabled = false;
+      toggleRainbowModeBtn.classList.remove("rainbow-mode-active");
+      return;
+    }
+    case "toggle": {
+      rainbowModeEnabled ? toggleRainbowMode("off") : toggleRainbowMode("on");
+      return;
+    }
+  }
+}
+
+function toggleShadeMode(stateStr) {
+  const affectedElements = [
+    paintColorPicker,
+    gridColorPicker,
+    eraseBtn,
+    toggleRainbowModeBtn,
+  ];
+
+  switch (stateStr) {
+    case "on": {
+      shadeModeEnabled = true;
+      toggleShadeModeBtn.classList.add("shade-mode-active");
+      disableElements(affectedElements);
+      return;
+    }
+    case "off": {
+      shadeModeEnabled = false;
+      toggleShadeModeBtn.classList.remove("shade-mode-active");
+      enableElements(affectedElements);
+      return;
+    }
+    case "toggle": {
+      shadeModeEnabled ? toggleShadeMode("off") : toggleShadeMode("on");
+      return;
+    }
+  }
+}
+
+function updateGridSizeDisplay(size) {
+  const display = `${size} x ${size}`;
+  gridSizeValue.innerText = display;
+}
+
+function updatePaintColorFromPicker(event) {
+  currentPaintColor = hexToRgbStr(event.target.value);
+  toggleRainbowMode("off");
+}
---
+++ utilities.js
@@ -0,0 +1,92 @@
+//########################## EXPORTED UTILS ############################
+export function hexToRgbStr(hex) {
+  if (hex.length === 4) {
+    hex = shortHexToFullHex(hex);
+  }
+
+  const r = parseInt(hex.slice(1, 3), 16);
+  const g = parseInt(hex.slice(3, 5), 16);
+  const b = parseInt(hex.slice(5, 7), 16);
+
+  return getRgbStrFromValues(r, g, b);
+}
+
+export function getRgbStrFromValues(r, g, b) {
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+export function getRandomRgbStr() {
+  const [randRed, randGreen, randBlue] = Array(3)
+    .fill()
+    .map((elem) => getRandInt(0, 256));
+  return getRgbStrFromValues(randRed, randGreen, randBlue);
+}
+
+export function getValuesFromRgbStr(rgbStr) {
+  const [r, g, b] = rgbStr.match(/\d{1,3}/g);
+  return { r: +r, g: +g, b: +b };
+}
+
+export function rgbStrToFullHex(rgbStr) {
+  const { r, g, b } = getValuesFromRgbStr(rgbStr);
+  return `#${valueToPaddedHex(r)}${valueToPaddedHex(g)}${valueToPaddedHex(b)}`;
+}
+
+/**
+ *Takes a single dom element or array of elements
+ *and applies the specified background color to them
+ *
+ * @param {object} elementsArr a single element or array of elements
+ * @param {string} rgbColorStr an rgb string formatted as: 'rgb(99, 255, 1)'
+ * */
+export function setElementsBgColor(elementsArr, rgbColorStr) {
+  if (typeof elementsArr !== "object") return;
+  if (!Array.isArray(elementsArr)) {
+    // if a single element is given, put it in an array
+    elementsArr = [elementsArr];
+  }
+  elementsArr.forEach(
+    (element) => (element.style.backgroundColor = rgbColorStr)
+  );
+}
+
+export function shadeRgbStrByFactor(rgbStr, factor) {
+  const { r, g, b } = getValuesFromRgbStr(rgbStr);
+  const [newR, newG, newB] = [r, g, b].map((val) => applyFactor(val));
+
+  function applyFactor(value) {
+    let newVal = value * factor;
+    newVal = newVal < 0 ? 0 : newVal;
+    newVal = newVal > 255 ? 255 : newVal;
+    return parseInt(newVal);
+  }
+
+  return `rgb(${newR}, ${newG}, ${newB})`;
+}
+
+//######################## NON EXPORTED UTILS ##########################
+/**
+ *
+ * @param {Number} min
+ * @param {Number} max
+ * @returns Number
+ *
+ * returns an integer between min (inclusive) and max (exclusive)
+ */
+function getRandInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function shortHexToFullHex(shortHex) {
+  const chunk1 = shortHex.slice(1, 2).repeat(2);
+  const chunk2 = shortHex.slice(2, 3).repeat(2);
+  const chunk3 = shortHex.slice(3, 4).repeat(2);
+
+  return "#" + chunk1 + chunk2 + chunk3;
+}
+
+function valueToPaddedHex(value) {
+  return value.toString(16).padStart(2, "0");
+}
